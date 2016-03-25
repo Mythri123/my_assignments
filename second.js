db2fb07471e7ad02c20e8633f5702adab9daf3ba
CHINNU <phaniukesht@gmail.com>
2016-03-25 08:39:19 -0500

    adding some files removing a script tag from html to be added late

---
+++ second.js
@@ -0,0 +1,17 @@
+//Closures
+var profile = "Phani_My3"
+function sayGn(nickname){
+	var wish = "Profile: " + nickname;
+	var gn = function(){
+		document.write(wish);
+	}
+	gn()
+}
+sayGn(profile);
+
+/* Closures are written to keep the function alive. 
+If we dont have closure then nickname which we are passing as an argument will not be avaliable tho wish is avaiable.
+And we know that js is full of functions. There are some functionality where an event (or lets say browser) 
+is not actually triggered to call a function,  
+to update something at this point
+ we look forward for a concept called closure. */
