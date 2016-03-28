97af170928be28ad0b9fdd13fa93f7212076a2ee
CHINNU <phaniukesht@gmail.com>
2016-03-28 12:02:19 -0500

    adding some other concepts

--- second.js
+++ second.js
@@ -1,19 +1,24 @@
-//Closures
-var profile = "Phani_My3"
-function sayGn(nickname){
-	var wish = "Profile: " + nickname;
-	var gn = function(){
-		document.write(wish);
-	}
-	gn()
-}
-sayGn(profile);
+var patientInfo = "John";
 
-/* Closures are written to keep the function alive. 
-If we dont have closure then nickname which we are passing as an argument will not be avaliable tho wish is avaiable.
-And we know that js is full of functions. There are some functionality where an event (or lets say browser) 
-is not actually triggered to call a function,  
-to update something at this point
- we look forward for a concept called closure. */
-var n =10;
-console.log(n.length)
+function nameToChange(){
+	var patientInfoCorrected = "Johnson";
+	var displayMyPatientInfo = function(){
+		var test1 = document.getElementById("test").innerHTML.value;
+		document.getElementById("test").innerHTML = ("Name changed to "+"<b>"+patientInfoCorrected + " From " + patientInfo+"</b>"+" on "+Date());
+	}
+	displayMyPatientInfo();
+}
+
+
+var TryThisForProto = function(){
+	this.someName = "Inserting some value to this function";
+}
+
+TryThisForProto.prototype.toPrintName = function(){
+	document.write("This is written in prototype function")
+	document.write(this.someName)
+}
+
+var someOtherVariableName = new TryThisForProto();
+
+someOtherVariableName.toPrintName();
