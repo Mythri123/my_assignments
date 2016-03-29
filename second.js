3a308bd187b9ee044b075745df2c7c05ace5881c
CHINNU <phaniukesht@gmail.com>
2016-03-29 17:32:32 -0500

    little recurssion code in js

--- second.js
+++ second.js
@@ -23,6 +23,20 @@ var someOtherVariableName = new TryThisForProto();
 someOtherVariableName.toPrintName();
 
 
-function calcAvgHeight(data){
-	
+
+//recurssion function
+
+function findSolution(target) {
+	function find(start, history) {
+		if (start == target)
+			return history;
+		else if (start > target)
+			return null;
+		else
+			return find(start + 5, "(" + history + " + 5)") ||
+				find(start * 3, "(" + history + " * 3)");
+	}
+	return find(1, "1");
 }
+
+console.log(findSolution(24));
